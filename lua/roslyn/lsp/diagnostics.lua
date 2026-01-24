3fcca6bdc30270793fdc85893e791f52718d3cde
Kemal YILDIRIM <kemal.yildirim@racelogic.co.uk>
2026-01-24 11:12:29 +0000

    text document fix for razor

--- lua/roslyn/lsp/diagnostics.lua
+++ lua/roslyn/lsp/diagnostics.lua
@@ -4,12 +4,15 @@ local M = {}
 function M.refresh(client)
     for buf in pairs(client.attached_buffers) do
         if vim.api.nvim_buf_is_loaded(buf) then
-            client:request(
-                vim.lsp.protocol.Methods.textDocument_diagnostic,
-                { textDocument = vim.lsp.util.make_text_document_params(buf) },
-                nil,
-                buf
-            )
+            local filetype = vim.api.nvim_get_option_value("filetype", { buf = buf })
+            if filetype == "cs" then
+                client:request(
+                    vim.lsp.protocol.Methods.textDocument_diagnostic,
+                    { textDocument = vim.lsp.util.make_text_document_params(buf) },
+                    nil,
+                    buf
+                )
+            end
         end
     end
 end
